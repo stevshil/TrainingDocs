6652d9a0a3167329b5a85605b250f4ba2b713f68
Steve Shilling <6329361+stevshil@users.noreply.github.com>
2025-08-06 11:01:11 +0100

    multiple data sets

--- Web/JavaScript/chartjs/mychart.js
+++ Web/JavaScript/chartjs/mychart.js
@@ -4,11 +4,18 @@ const ctx = document.getElementById('myChart');
     type: 'bar',
     data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
+      datasets: [
+        {
         label: '# of Votes',
         data: [12, 19, 3, 5, 2, 3],
         borderWidth: 1
-      }]
+        },
+        {
+          label: '# of Non-votes',
+          data: [4, 18, 3, 2],
+          borderWidth: 1
+        }
+    ]
     },
     options: {
       scales: {
